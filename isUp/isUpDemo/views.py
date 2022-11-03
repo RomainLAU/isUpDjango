ba8cbb6d44bcf26b5a6da49dbc77de71f1420579
RomainLAU <romain.laurent@epitech.digital>
2022-11-03 16:47:10 +0100

    Commit

--- isUp/isUpDemo/views.py
+++ isUp/isUpDemo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(["GET"])
 def index(request):
-
-    return render(request, "index.html")
+    return Response({"Hello": "world."})
