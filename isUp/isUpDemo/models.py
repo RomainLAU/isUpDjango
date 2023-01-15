f52985b54dad14c6322c032083567a2fd8a9f84a
RomainLAU <romain.laurent@epitech.digital>
2023-01-15 16:23:26 +0100

    Add routes to manage users

--- isUp/isUpDemo/models.py
+++ isUp/isUpDemo/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Domain(models.Model):
     domain = models.CharField(max_length=253)
     is_up = models.BooleanField(null=True, blank=True)
+    # owner = models.ForeignKey()
+
+
+class Check:
+    status = models.BooleanField()
+    last_check = models.DateTimeField()
